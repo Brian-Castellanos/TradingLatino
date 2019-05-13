5c70010fa40cb4a2e4791785d58158e771070138
Brian Enrique Castellanos Vazquez <romix_taguer@live.com.mx>
2019-05-13 01:30:46 -0500

    Maquetando Index

---
+++ TradingLatino/Presentacion/js/sitio.js
@@ -0,0 +1,120 @@
+/*------------------------
+INICIAMOS WOW
+-------------------------*/
+new WOW().init();
+
+/*----------------------------------
+Iniciamos smoothScroll (Scroll Suave)
+--------------------------------*/
+smoothScroll.init({
+    speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+    offset: 100, // Integer. How far to offset the scrolling anchor location in pixels
+
+});
+
+/*---------------------------------
+    OCULTAR Y MOSTRAR BOTON IR ARRIBA
+ ----------------------------------*/
+$(function () {
+    $(window).scroll(function () {
+        var scrolltop = $(this).scrollTop();
+        if (scrolltop >= 50) {
+            $(".ir-arriba").fadeIn();
+        } else {
+            $(".ir-arriba").fadeOut();
+        }
+    });
+
+});
+
+/*---------------------------------
+   CABECERA ANIMADA
+ ----------------------------------*/
+$(window).scroll(function () {
+
+    var nav = $('.encabezado');
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 80) {
+        nav.addClass("fondo-menu");
+    } else {
+        nav.removeClass("fondo-menu");
+    }
+});
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
+    document.body.appendChild(css);
+};
+
+$(document).ready(function () {
+
+    setTimeout(addExperiencias, 3000);
+
+    var pDinamico =
+        "<p class='parrafo'>- 9 años como profesional en tecnologías de la información.</p>" +
+        "<p class='parrafo'>- 4 años de Experiencia como Trader en Criptodivisas.</p>" +
+        "<p class='parrafo'>- 2 años de como Coach de inversión en cripto divisas.</p>" +
+        "<p class='parrafo'>- Clases Online Personalizadas.</p>" +
+        "<p class='parrafo'>- Sólidos conocimientos de Ethereum ENS y Contratos Inteligentes.</p>" +
+        "<p class='parrafo'>- Experto en análisis técnico y fundamental.</p>" +
+        "<p class='parrafo'>- Conocimientos Avanzados de Elliot." +
+        "<p class='parrafo'>- Sólidos conocimientos de Psicología del cripto mercado.";
+
+    function addExperiencias() {
+        $('div.Experiencia').append(pDinamico);
+        $('.parrafo').addClass("wow fadeInDown");
+    }
+});
